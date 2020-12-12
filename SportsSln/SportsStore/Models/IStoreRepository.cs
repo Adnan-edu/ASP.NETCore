79109bc162ac07545f6359881e5b8192556cd074
Adnan <md.ashikadnan@gmail.com>
2020-12-12 13:05:17 +1100

    Added database configuration in startup class and IStoreRepository repository interface

---
+++ SportsSln/SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+   /* A class that depends on the IProductRepository interface can obtain Product objects without needing to know the details of
+how they are stored or how the implementation class will deliver them.*/
+    public interface IStoreRepository
+    {
+        /*        This interface uses IQueryable<T> to allow a caller to obtain a sequence of Product objects.The IQueryable<T> interface is
+        derived from the more familiar IEnumerable<T> interface and represents a collection of objects that can be queried, such as those
+        managed by a database.*/
+        IQueryable<Product> Products { get; }
+    }
+}
--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -7,16 +7,36 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+ /*       The constructor receives an IConfiguration object through its
+constructor and assigns it to the Configuration property, which is used to access the connection string.*/
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        /*The IConfiguration interface provides access to the ASP.NET Core configuration system, which includes the contents of the
+appsettings.json file*/
+        private IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+/*            Entity Framework Core is configured with the AddDbContext method, which registers the database context class and configures
+the relationship with the database.The UseSQLServer method declares that SQL Server is being used and the connection string is
+read via the IConfiguration object.*/ 
+            services.AddDbContext<StoreDbContext>(opts => {
+                opts.UseSqlServer(
+                Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
